fb1ee7e38d9b376ecb8fadbb9d5f5f7fdc53a9a9
Oser <alex.mellado92@gmail.com>
2020-07-03 21:19:41 -0400

    Agregado comando 1020

--- esx_oserChat/client.lua
+++ esx_oserChat/client.lua
@@ -35,8 +35,9 @@ RegisterCommand('entorno', function(source, args, rawCommand)
 end, false)
 
 RegisterNetEvent('esx_oserChat:entornoSend')
-AddEventHandler('esx_oserChat:entornoSend', function(messageFull, messageSending)
+AddEventHandler('esx_oserChat:entornoSend', function(messageFull)
     if PlayerData.job.name == 'police' then
+    	--sonido de advertencia
 		TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.9, 'alert', 0.9)
 		TriggerEvent('chat:addMessage', messageFull)
     end
@@ -63,6 +64,63 @@ AddEventHandler('esx_oserChat:entornoMarker', function(targetCoords, entornoMarc
     	EndTextCommandSetBlipName(call)
 		while alpha ~= 0 do
 			Citizen.Wait(100 * 4)
+			alpha = alpha - 1
+			SetBlipAlpha(call, alpha)
+
+			if alpha == 0 then
+				RemoveBlip(call)
+				return
+			end
+        end
+    end
+end)
+
+--/1020 solo pueden usarlo si es policia
+RegisterCommand('1020', function(source, args, rawCommand)
+    if PlayerData.job.name == 'police' then
+    	local playerCoords = GetEntityCoords(PlayerPedId())
+		streetName,_ = GetStreetNameAtCoord(playerCoords.x, playerCoords.y, playerCoords.z)
+	    streetName = GetStreetNameFromHashKey(streetName)
+		local msg = rawCommand:sub(8)
+		entornoContador = entornoContador + 1
+	    TriggerServerEvent('esx_oserChat:1020',{
+	        x = ESX.Math.Round(playerCoords.x, 1),
+	        y = ESX.Math.Round(playerCoords.y, 1),
+	        z = ESX.Math.Round(playerCoords.z, 1)
+	    }, msg, streetName, entornoContador)
+    end
+end, false)
+
+RegisterNetEvent('esx_oserChat:1020Send')
+AddEventHandler('esx_oserChat:1020Send', function(messageFull)
+    if PlayerData.job.name == 'police' then
+    	--sonido de advertencia
+		TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.9, 'alert', 0.9)
+		TriggerEvent('chat:addMessage', messageFull)
+    end
+end)
+
+RegisterNetEvent('esx_oserChat:1020Marker')
+AddEventHandler('esx_oserChat:1020Marker', function(targetCoords, nombre)
+    if PlayerData.job.name == 'police' then
+        local alpha = 250
+        local call = AddBlipForCoord(targetCoords.x, targetCoords.y, targetCoords.z)
+        lugarAviso = "Ubicación del oficial: " .. nombre
+		SetBlipSprite (call, 458)
+		SetBlipDisplay(call, 4)
+		SetBlipScale  (call, 1.2)
+        SetBlipAsShortRange(call, true)
+        SetBlipAlpha(call, alpha)
+
+        SetBlipHighDetail(call, true)
+		SetBlipAsShortRange(call, true)
+
+		SetBlipColour (call, 69)
+		BeginTextCommandSetBlipName('STRING')
+		AddTextComponentString(lugarAviso)
+    	EndTextCommandSetBlipName(call)
+		while alpha ~= 0 do
+			Citizen.Wait(100 * 6)
 			alpha = alpha - 1
 			SetBlipAlpha(call, alpha)
 
--- esx_oserChat/server.lua
+++ esx_oserChat/server.lua
@@ -25,17 +25,31 @@ AddEventHandler('esx_oserChat:entorno', function(targetCoords, msg, streetName, 
     local xPlayers = ESX.GetPlayers()
 	local messageFull
     local entornoMarcador
-	local messageSending
     entornoMarcador = entornoContador
 	messageFull = {
-		template = '<div style="color: #FFFFFF; padding: 8px; margin: 5px; background-color: rgba(255, 0, 0, 1); border-radius: 5px;"><i class="fas fa-bell"style="font-size:15px"></i>  [Entorno] #{0} {1}: {2}</font></i></b></div>',
+		template = '<div style="color: #FFFFFF; padding: 8px; margin: 5px; background-color: rgba(255, 0, 0, 1); border-radius: 5px;"><i class="fas fa-bell"style="font-size:15px; font-weight:bold"></i>  [Entorno] #{0} {1}: {2}</font></i></b></div>',
 		args = {entornoContador, streetName, msg}
-	}
-	messageSending = {
-		template = '<div style="color: #FFFFFF; padding: 8px; margin: 5px; background-color: rgba(255, 0, 0, 1); border-radius: 5px;"><i class="fas fa-bell"style="font-size:15px"></i>  [Entorno] Mensaje enviado a las autoridades</font></i></b></div>',
-		args = {}
 	}
 	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Mensaje enviado' })
 	TriggerClientEvent('esx_oserChat:entornoMarker', -1, targetCoords, emergency, entornoMarcador)
-    TriggerClientEvent('esx_oserChat:entornoSend', -1, messageFull, messageSending)
+    TriggerClientEvent('esx_oserChat:entornoSend', -1, messageFull)
+end)
+
+RegisterServerEvent('esx_oserChat:1020')
+AddEventHandler('esx_oserChat:1020', function(targetCoords, msg, streetName, entornoContador)
+    local _source = source
+    local xPlayers = ESX.GetPlayers()
+    local pName= getIdentity(source)
+	fal = pName.firstname .. " " .. pName.lastname
+	local messageFull
+    local entornoMarcador
+    local nombre
+    nombre = fal
+    entornoMarcador = entornoContador
+	messageFull = {
+		template = '<div style="color: #FFFFFF; padding: 8px; margin: 5px; background-color: rgba(32, 162, 0, 1); border-radius: 5px;"><i class="fas fa-bell"style="font-size:15px; font-weight:bold"></i>  [10-20] Oficial: {0}. Ubicación: {1}</font></i></b></div>',
+		args = {fal, streetName}
+	}
+	TriggerClientEvent('esx_oserChat:1020Marker', -1, targetCoords,  nombre)
+    TriggerClientEvent('esx_oserChat:1020Send', -1, messageFull)
 end)
